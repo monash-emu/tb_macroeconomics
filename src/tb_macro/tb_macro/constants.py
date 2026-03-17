1395b576c605800786b989b63a59db2009115d89
James Trauer <james.trauer@monash.edu>
2026-03-17 12:07:44 +1100

    Add reinfection process

---
+++ src/tb_macro/tb_macro/constants.py
@@ -0,0 +1,5 @@
+
+LATENT_COMPS = ["incipient", "contained", "cleared"]
+ACTIVE_COMPS = ["subclin_lowinf", "clin_lowinf", "subclin_inf", "clin_inf"]
+OTHER_COMPS = ["mtb_naive", "treatment", "recovered"]
+REINFECT_COMPS = ["contained", "cleared", "recovered"]
