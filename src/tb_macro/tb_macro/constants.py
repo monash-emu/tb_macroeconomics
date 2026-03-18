176c61357e5891b205110abc51a67aac1947cb79
James Trauer <james.trauer@monash.edu>
2026-03-18 13:55:55 +1100

    Remove looping approach to infectiousness stratification

--- src/tb_macro/tb_macro/constants.py
+++ src/tb_macro/tb_macro/constants.py
@@ -1,5 +1,5 @@
 
 LATENT_COMPS = ["incipient", "contained", "cleared"]
-ACTIVE_COMPS = ["subclin_lowinf", "clin_lowinf", "subclin_inf", "clin_inf"]
+ACTIVE_COMPS = ["subclin", "clin"]
 OTHER_COMPS = ["mtb_naive", "treatment", "recovered"]
 REINFECT_COMPS = ["contained", "cleared", "recovered"]
