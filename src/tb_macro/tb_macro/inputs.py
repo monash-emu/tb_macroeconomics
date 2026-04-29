3501ba7669f25c6f1ae311ef4a80c84212eb8a88
jtrauer <james.trauer@monash.edu>
2026-04-29 22:10:58 +1000

    Mortality loading function

--- src/tb_macro/tb_macro/inputs.py
+++ src/tb_macro/tb_macro/inputs.py
@@ -1,7 +1,7 @@
 from typing import List
 import pandas as pd
 
-from tb_macro.constants import DATA_PATH
+from tb_macro.constants import DATA_PATH, AGE_STRATA, MAX_AGE
 
 
 def get_country_pop(
@@ -21,14 +21,12 @@ def get_country_pop(
 
 def get_single_age_pop_from_ungroups(
     data: pd.DataFrame,
-    max_age: int,
 ) -> pd.DataFrame:
     """Get population data in single year rows
     using UN data from get_country_pop.
 
     Args:
         data: Output of get_country_pop
-        max_age: The highest age to go to for the top age group
 
     Returns:
         The single-age data
@@ -39,7 +37,7 @@ def get_single_age_pop_from_ungroups(
         agegrp = r["AgeGrp"]
         if agegrp.endswith("+"):
             a0 = int(agegrp[:-1])
-            a1 = max_age
+            a1 = MAX_AGE
         else:
             a0, a1 = map(int, str(agegrp).split("-"))
 
@@ -52,24 +50,20 @@ def get_single_age_pop_from_ungroups(
 
 def get_group_popsizes(
     single_age_pops: pd.DataFrame,
-    age_strata: List[int],
-    max_age: int,
 ) -> pd.DataFrame:
     """Get dataframe for age group populations by year.
 
     Args:
         single_age_pops: The single age population data from
             get_single_age_pop_from_ungroups
-        age_strata: The age groups
-        max_age: The highest age to go to for the top age group
 
     Returns:
         The dataframe with rows for years and columns for age groups
     """
     single_age_pops["Age Group"] = pd.cut(
         single_age_pops["Age"],
-        bins=age_strata + [max_age],
-        labels=age_strata,
+        bins=AGE_STRATA + [MAX_AGE],
+        labels=AGE_STRATA,
         right=False,
     )
     return (
@@ -77,3 +71,34 @@ def get_group_popsizes(
         .sum()
         .unstack("Age Group")
     )
+
+
+def get_un_mortality(
+    iso3: str,
+    start_year: int,
+) -> pd.DataFrame:
+    """Get UN mortality data for a country after
+    a specified year.
+
+    Args:
+        iso3: The country identifier
+        start_year: The year to start the data from
+
+    Returns:
+        Dataframe with columns for age groups and rows for years
+    """
+    mort_data = pd.read_csv(DATA_PATH / "population/un_mortality.csv")
+    relevant_cols = ["Time", "AgeGrp", "DeathTotal"]
+    country_filt = mort_data["ISO3_code"] == iso3
+    time_filt = mort_data["Time"] > start_year
+    mort_data = mort_data.loc[country_filt & time_filt, relevant_cols]
+    mort_data["DeathTotal"] *= 1000.0  # convert from thousands
+    mort_data["age"] = mort_data["AgeGrp"].str.replace("100+", "100").astype(int)
+    mort_data["Age Group"] = pd.cut(
+        mort_data["age"],
+        bins=AGE_STRATA + [MAX_AGE],
+        labels=AGE_STRATA,
+        right=False,
+    )
+    mort_data = mort_data.groupby(["Time", "Age Group"], as_index=False).sum()
+    return mort_data.pivot(index="Time", columns="Age Group", values="DeathTotal")
