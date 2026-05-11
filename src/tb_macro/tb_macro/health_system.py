3a435dd3e949f7687f6c6edd67f170be7016bc11
James Trauer <james.trauer@monash.edu>
2026-05-11 11:50:05 +1000

    Package treatment outcome flows into new file

---
+++ src/tb_macro/tb_macro/health_system.py
@@ -0,0 +1,101 @@
+from jax import numpy as jnp
+import pandas as pd
+
+from summer3.epi import TransitionFlow, CompartmentalModelODE, Stratification
+from summer3.graph import defer, Time, Parameter
+
+from tb_macro.demography import make_death_func
+from tb_macro.constants import AGE_STRATA
+
+
+def get_rx_outcome_rate(
+    rx_duration: float,
+    tsr: float,
+    prop_neg_rx_death: float,
+    time: float,
+    death_times: jnp.array,
+    death_data: jnp.array,
+    start_time: float,
+    outcome: str,
+) -> float:
+    """Get the treatment outcome rate for 
+    relapse, treatment-related death or success.
+
+    Args:
+        rx_duration: Treatment duration
+        tsr: Treatment success "rate" (a proportion)
+        prop_neg_rx_death: Proportion of unsuccessful treatment outcomes 
+            resulting in death
+        time: Model time
+        death_times: The per capita death rates
+        death_data: The corresponding times for these rates
+        start_time: The model starting time as a calendar year
+        outcome: The outcome of interest
+
+    Returns:
+        The rate value for the outcome of interest
+    """
+    death_func = make_death_func(death_times, death_data, start_time)
+    death_rates = death_func(time)
+
+    prop_natural_death_on_rx = 1.0 - jnp.exp(-rx_duration * death_rates)
+    req_prop_death_on_rx = (1.0 - tsr) * prop_neg_rx_death
+    prop_death_from_rx = jnp.maximum(req_prop_death_on_rx - prop_natural_death_on_rx, 0.0)
+    prop_total_death = prop_death_from_rx + prop_natural_death_on_rx
+
+    relapse_prop = jnp.maximum(1.0 - tsr - prop_total_death, 0.0)
+    success = jnp.maximum(1.0 - relapse_prop - prop_total_death, 0.0)
+
+    if outcome == "relapse":
+        return relapse_prop / rx_duration
+    elif outcome == "rx_death":
+        return prop_death_from_rx / rx_duration
+    elif outcome == "success":
+        return success / rx_duration
+    
+
+def add_treatment_flows(
+    death_rates: pd.DataFrame,
+    start_time: float,
+    epi_model: CompartmentalModelODE,
+    disease_state: Stratification,
+    age_strat: Stratification,
+    infect_strat: Stratification,
+    clin_strat: Stratification,
+):
+    """Add treatment-related outcome flows to epi model.
+
+    Args:
+        death_rates: The death rate data
+        start_time: The model starting time as a calendar year
+        epi_model: The epidemiological model to add the flows to
+        disease_state: The compartmental stratification object
+        age_strat: The age stratification object
+        infect_strat: The infectiousness stratification
+        clin_strat: The clinical stratification
+    """
+    for age in AGE_STRATA:
+        rx_source = (disease_state["treatment"], age_strat[str(age)])
+        rx_dests = {
+            "relapse": (clin_strat["subclin"], infect_strat["low"], age_strat[str(age)]),
+            "rx_death": (disease_state["mtb_naive"], age_strat["0"]),
+            "success": (disease_state["recovered"], age_strat[str(age)]),
+        }
+        for outcome in rx_dests:
+            outcome_flow = TransitionFlow(
+                f"{outcome}_{age}",
+                rx_source,
+                rx_dests[outcome],
+                defer(get_rx_outcome_rate)(
+                    Parameter("rx_duration", 0.0), 
+                    Parameter("tsr", 0.0), 
+                    Parameter("prop_neg_rx_death", 0.0), 
+                    Time, 
+                    death_rates.index.to_numpy(dtype=float), 
+                    death_rates[age].to_numpy(dtype=float), 
+                    start_time,
+                    outcome,
+                ),
+            )
+            epi_model.add_flow(outcome_flow)
+            
