900cd4bd1dc4ac28e90c6f3ef73c5b64378a1143
James Trauer <james.trauer@monash.edu>
2026-07-16 16:00:53 +1000

    Move plotting files for single run to separate file

--- src/tb_macro/tb_macro/outputs.py
+++ src/tb_macro/tb_macro/outputs.py
@@ -1,12 +1,8 @@
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
 from pathlib import Path
 from datetime import datetime, UTC
 
 from summer3.epi import ManagedArray, Stratification
-
-from tb_macro.constants import AGE_STRATA, PREV_STATES, LATENT_STATES
 
 
 def get_total_pop(
@@ -58,45 +54,6 @@ def get_partial_strat_props(
     vals = results["compartments"].sumcats(compartment=strat.categories())
     strat_total = vals.sum(to_dims="time")
     return vals / strat_total
-
-
-def plot_dynamic_mixing_matrix(
-    dmm: ManagedArray,
-    start: float,
-    interval: float,
-    n_cols: int,
-):
-    """Plot dynamic mixing matrices in multipanel figure.
-
-    Args:
-        dmm: The dynamic mixing matrix computed value
-        start: The first year to plot
-        interval: The interval between years
-        n_cols: The number of columns for the plots
-            (determines the number of panels - will be n_cols x2)
-    """
-    n_rows = 2
-    figsize = [4 * n_cols, 7]
-    dmm_xa = dmm.to_xarray_da()
-    vmax = dmm_xa.max()
-    fig, axes = plt.subplots(n_rows, n_cols, figsize=figsize, constrained_layout=True)
-    flat_axes = axes.ravel()
-    for a, ax in enumerate(flat_axes):
-        year = start + a * interval
-        hm = sns.heatmap(
-            dmm_xa.sel(time=year),
-            cmap="viridis",
-            xticklabels=AGE_STRATA,
-            yticklabels=AGE_STRATA[::-1],
-            ax=ax,
-            vmin=0.0,
-            vmax=vmax,
-            cbar=False,
-        )
-        ax.set_title(int(year))
-    im = hm.collections[0]
-    fig.colorbar(im, ax=axes, shrink=0.8)
-    return fig
 
 
 def get_share_folder_file_path(
---
+++ src/tb_macro/tb_macro/plotting.py
@@ -0,0 +1,93 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from summer3.epi import Stratification, ManagedArray
+
+from tb_macro.constants import AGE_STRATA
+from tb_macro.outputs import get_complete_strat_props, get_partial_strat_props
+
+pd.options.plotting.backend = "matplotlib"
+
+
+def plot_comp_distributions(
+    results: dict,
+    disease_state: Stratification,
+    age_strat: Stratification,
+    infect_strat: Stratification,
+    clin_strat: Stratification,
+    plot_end_time: float,
+    group_popsize,
+) -> plt.figure:
+    """Plot the distribution of the population by compartment
+    or stratification.
+
+    Args:
+        results: Results from a single model run
+        disease_state: The compartment stratification
+        age_strat: The age stratification
+        infect_strat: The infectiousness stratification
+        clin_strat: The clinical stratification
+
+    Returns:
+        The figure
+    """
+    total_pop = results["compartments"].sum(to_dims="time")
+    dstate_props = get_complete_strat_props(results, disease_state)
+    age_vals = results["compartments"].sumcats(compartment=age_strat.categories())
+    age_props = get_complete_strat_props(results, age_strat)
+    inf_props = get_partial_strat_props(results, infect_strat)
+    clin_props = get_partial_strat_props(results, clin_strat)
+
+    fig, axes = plt.subplots(2, 3, figsize=[15, 7], sharex=True)
+    total_pop.to_pandas_df().plot.area(ax=axes[0, 0], title="total population versus target data", xlim=[1920, plot_end_time - 1], legend=False)
+    group_popsize.sum(axis=1).plot(ax=axes[0, 0], linewidth=0.0, color="k", marker="o", markersize=1.0)
+    dstate_props.to_pandas_df().clip(lower=0).plot.area(ax=axes[1, 0], title="disease state distribution", ylim=[0.0, 1.0])
+    age_vals.to_pandas_df().plot.area(ax=axes[0, 1], title="age group sizes")
+    age_props.to_pandas_df().plot.area(ax=axes[0, 2], title="age distribution", ylim=[0.0, 1.0])
+    clin_props.to_pandas_df().plot.area(ax=axes[1, 1], title="clinical status distribution", ylim=[0.0, 1.0])
+    inf_props.to_pandas_df().plot.area(ax=axes[1, 2], title="infectiousness status distribution", ylim=[0.0, 1.0])
+    for ax in axes.ravel():
+        ax.legend(loc="upper left")
+    plt.close()
+    return fig
+
+
+def plot_dynamic_mixing_matrix(
+    dmm: ManagedArray,
+    start: float,
+    interval: float,
+    n_cols: int,
+):
+    """Plot dynamic mixing matrices in multipanel figure.
+
+    Args:
+        dmm: The dynamic mixing matrix computed value
+        start: The first year to plot
+        interval: The interval between years
+        n_cols: The number of columns for the plots
+            (determines the number of panels - will be n_cols x2)
+    """
+    n_rows = 2
+    figsize = [4 * n_cols, 7]
+    dmm_xa = dmm.to_xarray_da()
+    vmax = dmm_xa.max()
+    fig, axes = plt.subplots(n_rows, n_cols, figsize=figsize, constrained_layout=True)
+    flat_axes = axes.ravel()
+    for a, ax in enumerate(flat_axes):
+        year = start + a * interval
+        hm = sns.heatmap(
+            dmm_xa.sel(time=year),
+            cmap="viridis",
+            xticklabels=AGE_STRATA,
+            yticklabels=AGE_STRATA[::-1],
+            ax=ax,
+            vmin=0.0,
+            vmax=vmax,
+            cbar=False,
+        )
+        ax.set_title(int(year))
+    im = hm.collections[0]
+    fig.colorbar(im, ax=axes, shrink=0.8)
+    plt.close()
+    return fig
