1fa09fc89b29c9029ef5b44b0d912ead98bffb65
James Trauer <james.trauer@monash.edu>
2026-07-16 16:07:52 +1000

    Move targets used in multiple notebooks to new file

---
+++ src/tb_macro/tb_macro/targets.py
@@ -0,0 +1,25 @@
+import pandas as pd
+
+# Data previously obtained from the Vietnam NTP by Long
+notif_target = pd.Series(
+    {
+        2011: 100518,
+        2012: 103906,
+        2013: 102196,
+        2014: 102087,
+        2015: 100780,
+        2016: 102097,
+        2017: 102725,
+        2018: 99658,
+        2019: 102503,
+        2020: 99582,
+        2021: 77657,
+        2022: 102479,
+        2023: 104517,
+    }
+)
+latent_target = pd.Series(
+    {
+        2019: 43.0,
+    }
+)
