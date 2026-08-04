1bc0414767566c3372b19f0530dc82b14f0fac2a
James Trauer <james.trauer@monash.edu>
2026-08-04 11:02:57 +1000

    Add test Massive scripts

---
+++ scripts/massive/calibrate.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#SBATCH --job-name=test_tb
+#SBATCH --account=sh30
+
+#SBATCH --time=00:05:00
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=1G
+
+cd $SCRIPT_DIR/../..
+
+pixi run python scripts/massive/test_massive.py
