90683cd1b11ba6513ed26e27e0c2df1524e0f92b
jtrauer <james.trauer@monash.edu>
2026-08-25 16:31:47 +1000

    Extend run time for remote calibrations

--- scripts/massive/calibrate.sh
+++ scripts/massive/calibrate.sh
@@ -3,7 +3,7 @@
 #SBATCH --job-name=calibrate_vnm
 #SBATCH --account=sh30
 
-#SBATCH --time=10:00:00
+#SBATCH --time=24:00:00
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=8
 #SBATCH --mem-per-cpu=4096
