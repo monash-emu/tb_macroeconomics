d52b8f0ab8fb5db59168902d470a9c4c79913d60
jtrauer <james.trauer@monash.edu>
2026-09-14 17:04:18 +1000

    Specify time limit along with iterations in calls for remote calibrations

--- scripts/massive/launch.sh
+++ scripts/massive/launch.sh
@@ -1,5 +1,3 @@
 #! /usr/bin/env bash
-
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-
-sbatch --export=SCRIPT_DIR=$SCRIPT_DIR "$SCRIPT_DIR/calibrate.sh" "$1"
+sbatch --time="$2" --export=SCRIPT_DIR="$SCRIPT_DIR" "$SCRIPT_DIR/calibrate.sh" "$1"
